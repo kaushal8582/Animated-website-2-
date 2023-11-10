2f2863803a3e57f5077b551b0834779a51958bf8
Kaushal Singh <137991463+kaushal8582@users.noreply.github.com>
2023-11-10 08:58:32 +0530

    Animate website using Shery js libary

---
+++ animated website 2/app.js
@@ -0,0 +1,90 @@
+Shery.imageEffect(".back", {
+  style: 5,
+  config: {
+    "a": { "value": 2, "range": [0, 30] },
+    "b": { "value": 0.75, "range": [-1, 1] },
+    "zindex": {
+      "value": -9996999,
+      "range": [-9999999, 9999999]
+    },
+    "aspect": { "value": 2.1241611399783085 },
+    "gooey": { "value": true },
+    "infiniteGooey": { "value": true },
+    "growSize": {
+      "value": 4,
+      "range": [1, 15]
+    },
+    "durationOut": { "value": 1, "range": [0.1, 5] },
+    "durationIn": { "value": 1.5, "range": [0.1, 5] },
+    "displaceAmount": { "value": 0.5 },
+    "masker": { "value": false },
+    "maskVal": { "value": 1, "range": [1, 5] },
+    "scrollType": { "value": 0 },
+    "geoVertex": { "range": [1, 64], "value": 1 },
+    "noEffectGooey": { "value": false },
+    "onMouse": { "value": 1 },
+    "noise_speed": { "value": 0.2, "range": [0, 10] },
+    "metaball": {
+      "value": 0.2, "range": [0, 2],
+      "_gsap": { "id": 3 }
+    },
+    "discard_threshold": {
+      "value": 0.5,
+      "range": [0, 1]
+    },
+    "antialias_threshold": { "value": 0, "range": [0, 0.1] },
+    "noise_height": { "value": 0.5, "range": [0, 2] },
+    "noise_scale": { "value": 10, "range": [0, 100] }
+  },
+  gooey: true,
+  /*optional parameters
+  these parameter dose not applies to custom scroll trigger callback */
+  scrollSnapping: true,
+  scrollSpeed: 6,
+  touchSpeed: 6,
+  damping: 7,
+});
+
+
+var elems = document.querySelectorAll(".elem");
+
+elems.forEach((elem) => {
+
+  var animation = false;
+  var h1s = elem.querySelectorAll("h1");
+  var index = 0;
+  document.querySelector("#main").addEventListener("click", () => {
+
+    if (!animation) {
+      animation = true;
+      gsap.to(h1s[index], {
+        top: "-=100%",
+        ease: Power1,
+        // duration:2,
+        onComplete: function () {
+          gsap.set(this._targets[0],
+            {
+              top: "100%"
+
+            })
+            animation = false;
+        }
+      })
+
+     
+
+      index >= h1s.length - 2 ? index = 0 : index++;
+
+      gsap.to(h1s[index], {
+        top: 0,
+        ease: Power3
+      })
+
+    }
+
+
+  })
+
+})
+
+
